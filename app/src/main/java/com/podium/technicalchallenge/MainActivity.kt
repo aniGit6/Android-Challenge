b6fb05d660aedab7957ee5842ec3e5cfa2f6988b
anikhalili <anikhalili50@gmail.com>
2024-05-14 01:22:14 -0700

    Cleanup

--- app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
+++ app/src/main/java/com/podium/technicalchallenge/MainActivity.kt
@@ -21,24 +21,6 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
-        navController.addOnDestinationChangedListener { _, destination, arguments ->
-            runOnUiThread {
-                if (destination.id == R.id.mobile_navigation) {
-                    destination.label = getString(R.string.movie_overview)
-                } else if (destination.id == R.id.movie_detail_fragment) {
-                    destination.label = getString(R.string.movie_details)
-                } else if (destination.id == R.id.genre_list_fragment) {
-                    destination.label = getString(R.string.browse_genre)
-                } else if (destination.id == R.id.movie_list_fragment) {
-                    val genre = arguments?.getString("genre")
-                    if (genre != null) {
-                        destination.label = getString(R.string.genre_movies, genre)
-                    } else {
-                        destination.label = getString(R.string.movies)
-                    }
-                }
-            }
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
